1bab7feb50192f73738768512248e49393d9d170
MiaaaaaaZ <mirongzou@gmail.com>
2019-08-24 23:28:23 +0000

    destroy forbidden

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   def index
     @places = Place.paginate(page: params[:page], per_page: 5)
@@ -34,6 +34,9 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
